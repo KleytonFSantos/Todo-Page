59227a74216135ab22cf66dae0e16e81a9b8ed69
kleytondev <akatsukipb12@gmail.com>
2022-06-21 22:39:10 -0300

    adicionando banco de dados

---
+++ lib/db.ts
@@ -0,0 +1,23 @@
+import { prisma } from "./prisma";
+
+export interface Todo {
+    id: number;
+    name: string;
+    description: string;
+    title: string;   
+}
+
+export async function getAllTodos(){
+    const data = await prisma.todo.findMany();
+    return data;
+}
+
+export async function createTodo(name:string, description: string, title:string){
+    await prisma.todo.create({
+        data: {
+            name,
+            description,
+            title,
+        },
+    });
+}
